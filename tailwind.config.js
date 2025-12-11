0d11c9d64f20e27cdc8e937e0f443db56d63781e
FlowlyNova <openminddia@gmail.com>
2025-12-11 14:43:09 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#06B6D4",
+        purpleBrand: "#8B5CF6"
+      }
+    },
+  },
+  plugins: [],
+}
